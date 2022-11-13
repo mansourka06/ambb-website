f10d124fb916e1c9f22634f3e78541acdfc5e951
Mansour ka <kamansour06@gmail.com>
2022-11-13 17:07:38 +0100

    update v1

--- script.js
+++ script.js
@@ -30,19 +30,18 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
     // typing text animation script
-    //var typed = new Typed(".typing", {
-    //    strings: ["Technologies"],
-    //    // strings: ["cloud", "DevOps", "Engineer"],
-    //    typeSpeed: 100,
-    //    backSpeed: 60,
-    //    loop: true
-    //});
-    var typed = new Typed(".typing-2", {
-        strings: ["Technologies"],
+    var typed = new Typed(".typing", {
+        strings: ["Innovation", "Digitalisation", "IT transformation"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
+    //var typed = new Typed(".typing-2", {
+    //    strings: ["Technologies"],
+    //   typeSpeed: 100,
+    //    backSpeed: 60,
+    //    loop: true
+    //});
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
